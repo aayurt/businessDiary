edb762405243e0eeb830c4ac701e2f444ca89c40
aayurt <aayurtshrestha@gmail.com>
2026-05-27 00:17:18 +0545

    New changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: process.env.NODE_ENV === "production" ? "standalone" : undefined,
+  output: "standalone",
   productionBrowserSourceMaps: false,
   compiler: {
     removeConsole: process.env.NODE_ENV === "production"
